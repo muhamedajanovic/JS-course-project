83adff2d94d6caef69000a98ca451c20ec7e932d
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2022-07-24 00:48:32 +0200

    005 Values and Variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,5 +1,11 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
+// if (js === "amazing") alert("JavaScript is FUN!");
 
 let result = 40 + 8 + 23 - 10;
 console.log(result);
+
+console.log("Muhamed");
+console.log(21);
+
+let firstName = "Muhamed";
+console.log(firstName);
