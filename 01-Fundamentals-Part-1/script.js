c348764a50d253a167a938c76b0cb0a156caa6de
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2022-07-24 12:17:16 +0200

    007 Data Types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -9,3 +9,15 @@ console.log(21);
 
 let firstName = "Muhamed";
 console.log(firstName);
+
+let javascirptIsFun = true;
+
+console.log(javascirptIsFun);
+
+console.log(typeof 41);
+console.log(typeof "Muhamed");
+console.log(typeof javascirptIsFun);
+console.log(typeof true);
+
+javascirptIsFun = "Yes";
+console.log(typeof javascirptIsFun);
