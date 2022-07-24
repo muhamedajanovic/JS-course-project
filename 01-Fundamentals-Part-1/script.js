da814467d1c08b5d62f3e6dbdf3317bf75e7a625
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2022-07-24 12:30:02 +0200

    008 let. const, var

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -21,3 +21,13 @@ console.log(typeof true);
 
 javascirptIsFun = "Yes";
 console.log(typeof javascirptIsFun);
+
+// reassigning the value of a variable
+let age = 30;
+age = 31;
+
+// variables thats not gonna change
+const birthYear = 2001;
+// birthYear = 2002; not working
+
+// var is the old way of assigning variables
