d7ff79c533fad302de6a0dbf5300ed9976eba148
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2022-07-27 22:19:26 +0200

    003 Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,7 +1,19 @@
 "use strict";
 
-let hasDriversLicence = false;
-const passTest = true;
+// let hasDriversLicence = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicence = true;
-if (hasDriversLicence) console.log(`I can drive`);
+// if (passTest) hasDriversLicence = true;
+// if (hasDriversLicence) console.log(`I can drive`);
+
+function logger() {
+  console.log(`Ide gas`);
+}
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples, and ${oranges} oranges.`;
+  return juice;
+}
+
+fruitProcessor(5, 0);
