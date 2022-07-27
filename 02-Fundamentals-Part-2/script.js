779b709620f81116e0d3f0600775d89c5b259d53
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2022-07-27 22:24:49 +0200

    004 Function Declarations vs. Expressions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -6,14 +6,29 @@
 // if (passTest) hasDriversLicence = true;
 // if (hasDriversLicence) console.log(`I can drive`);
 
-function logger() {
-  console.log(`Ide gas`);
+// function logger() {
+//   console.log(`Ide gas`);
+// }
+
+// function fruitProcessor(apples, oranges) {
+//   console.log(apples, oranges);
+//   const juice = `Juice with ${apples} apples, and ${oranges} oranges.`;
+//   return juice;
+// }
+
+// fruitProcessor(5, 0);
+
+function calcAge1(birthYear) {
+  const age = 2037 - birthYear;
+  return age;
 }
 
-function fruitProcessor(apples, oranges) {
-  console.log(apples, oranges);
-  const juice = `Juice with ${apples} apples, and ${oranges} oranges.`;
-  return juice;
-}
+const age1 = calcAge1(1991);
+console.log(age1);
 
-fruitProcessor(5, 0);
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const age2 = calcAge2(1991);
+console.log(age1, age2);
