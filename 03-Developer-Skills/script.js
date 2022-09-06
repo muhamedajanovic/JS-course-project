f8060c05e0052248b048c29bc9a5cfa6ac61a34d
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2022-09-06 16:14:16 +0200

    009 Debugging with the Console and Breakpoints

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,19 +1,35 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
-const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+// const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 
-const calcTempApmlitude = function (temps) {
-  let max = temps[0];
-  let min = temps[0];
-  for (let i = 0; i < temps.length; i++) {
-    if (typeof temps[i] !== "number") continue;
-    if (temps[i] > max) max = temps[i];
-    if (temps[i] < min) min = temps[i];
-  }
-  console.log(max, min);
-  return max - min;
+// const calcTempApmlitude = function (temps) {
+//   let max = temps[0];
+//   let min = temps[0];
+//   for (let i = 0; i < temps.length; i++) {
+//     if (typeof temps[i] !== "number") continue;
+//     if (temps[i] > max) max = temps[i];
+//     if (temps[i] < min) min = temps[i];
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+
+// const amplitude = calcTempApmlitude(temperatures);
+// console.log(amplitude);
+
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    value: Number(prompt("Degree celsius:")),
+  };
+  console.table(measurement);
+  const kelvin = measurement.value + 273;
+  return kelvin;
 };
 
-const amplitude = calcTempApmlitude(temperatures);
-console.log(amplitude);
+console.log(measureKelvin());
+
+// console.warn("ayan");
+// console.error("ayan");
