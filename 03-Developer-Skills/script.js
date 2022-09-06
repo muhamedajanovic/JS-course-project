b82a514d0956e5c9a5ebbe1a100889c741fc7638
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2022-09-06 15:25:52 +0200

    007 Using Google, StackOverflow and MDN

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,2 +1,19 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
+
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
+const calcTempApmlitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    if (typeof temps[i] !== "number") continue;
+    if (temps[i] > max) max = temps[i];
+    if (temps[i] < min) min = temps[i];
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitude = calcTempApmlitude(temperatures);
+console.log(amplitude);
