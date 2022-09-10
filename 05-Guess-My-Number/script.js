0d2530a90ee70c2abcd57cfe8d0525e7a8ed5248
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2022-09-10 02:02:54 +0200

    007 Implementing the Game Logic

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -8,9 +8,33 @@
 // document.querySelector(".guess").value = 23;
 // console.log(document.querySelector(".guess").value);
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector(".number").textContent = secretNumber;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   if (!guess) {
     document.querySelector(".message").textContent = "No number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct number!";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost!";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost!";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
