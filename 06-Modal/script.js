587bfb0b9f1b76e2cc504200ef302eb9c5c2f4dd
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2022-09-11 23:34:10 +0200

    013 Working With Classes

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -5,8 +5,19 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
+
+modal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
