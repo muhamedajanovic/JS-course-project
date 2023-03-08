cef1d59d1b26b42b4f0d9381993d067c5f53dd72
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2023-03-08 19:57:24 +0100

    004 Destructuring Objects

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -16,6 +16,10 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
+  orderDelivery: function (obj) {
+    console.log(obj);
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -31,32 +35,64 @@ const restaurant = {
     },
   },
 };
+restaurant.orderDelivery({
+  time: "22:30",
+  address: " Via del Sole, 21",
+  mainIndex: 2,
+  starterIndex: 2,
+});
 
-const arr = [2, 3, 4];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
 
-const [x, y, z] = arr;
-console.log(x, y, z);
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
 
-let [main, , secondary] = restaurant.categories;
-console.log(main, secondary);
+const { menu = [], starterMenu: starters = [] } = restaurant;
 
-const temp = main;
-main = secondary;
-secondary = temp;
-console.log(main, secondary);
+console.log(menu, starters);
 
-[main, secondary] = [secondary, main];
-console.log(main, secondary);
+let a = 111;
+let b = 999;
+const obj = { a: 14, b: 7, c: 14 };
 
-const [starter, mainCourse] = restaurant.order(2, 0);
-console.log(starter, mainCourse);
+({ a, b } = obj);
+console.log(a, b);
 
-const nested = [2, 4, [5, 6]];
-// const [i, , j] = nested;
-// console.log(i, j);
+const {
+  fri: { open, close },
+} = openingHours;
+console.log(open, close);
 
-const [i, , [j, k]] = nested;
-console.log(i, j, k);
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
+
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
+
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+// const nested = [2, 4, [5, 6]];
+// // const [i, , j] = nested;
+// // console.log(i, j);
+
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
